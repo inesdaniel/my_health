18b0dd77f2feaa445c8733699c5a95c39fb718af
Ines Daniel <i.danie8@gmail.com>
2018-08-05 19:44:51 -0500

    create users, fix authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :shots
   has_many :vitals
   has_many :lab_tests
   has_many :exams
   has_many :meds
-
-  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_002428) do
+ActiveRecord::Schema.define(version: 2018_08_06_003039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2018_08_02_002428) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
